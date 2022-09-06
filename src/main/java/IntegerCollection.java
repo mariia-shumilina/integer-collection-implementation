be908e08fafb6d52243c4ff12821b2654be5f8cb
Mariia Shumilina <ShumilinaMary0@gmail.com>
2022-09-07 00:22:45 +0300

    created basic methods for collection management

--- src/main/java/IntegerCollection.java
+++ src/main/java/IntegerCollection.java
@@ -1,30 +1,80 @@
-/* Контейнер должен позволять добавлять, извлекать, удалять элементы */
+/*
+ * Создать класс контейнер, позволяющий хранить произвольное количество целых чисел.
+ * Использование встроенных коллекций запрещено.
+ * Задание можно реализовать с помощью массива или связанного списка.
+ * Контейнер должен позволять добавлять, извлекать, удалять элементы.
+ * */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static java.lang.Math.max;
 
 public class IntegerCollection {
     private int[] data;
-    private int amountOfElements;
-    private int capacity;
+    private int size;
     private static final int DEFAULT_CAPACITY = 15;
 
     public IntegerCollection() {
-        capacity = DEFAULT_CAPACITY;
-        amountOfElements = 0;
+        data = new int[DEFAULT_CAPACITY];
+        size = 0;
     }
 
     public IntegerCollection(int wantedCapacity) {
-        capacity = wantedCapacity;
-        data = new int[capacity];
-        amountOfElements = 0;
+        data = new int[wantedCapacity];
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    protected void expand() {
+        int[] biggerArray = Arrays.copyOf(data, data.length >> 1);
+        data = biggerArray;
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    protected void squeezeIfNeeded() {
+        if (data.length > size * 3) {
+            int[] smallerArray = Arrays.copyOf(data, data.length * 2 / 3);
+            data = smallerArray;
+        }
     }
 
     public IntegerCollection(int... elements) {
-        amountOfElements = elements.length;
-        capacity = max(DEFAULT_CAPACITY, amountOfElements);
-        data = Arrays.copyOf(elements, amountOfElements);
+        size = elements.length;
+        int capacity = max(DEFAULT_CAPACITY, size);
+        data = Arrays.copyOf(elements, capacity);
+    }
+
+    public void add(int element) {
+        if (size + 1 >= data.length) {
+            expand();
+        }
+        data[size++] = element;
+    }
+
+    public int get(int index) {
+        if (index < 0 && index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return data[index];
+    }
+
+    public void delete(int index) {
+        if (index < 0 && index > size && empty()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+
+        size--;
     }
 }
 
